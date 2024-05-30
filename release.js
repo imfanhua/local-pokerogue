28a7f519a9bc58b6baf8df32748768d00c1c8cd9
FanHua <i@fanhua.me>
2024-05-31 04:59:11 +0800

    fix: release.js

--- release.js
+++ release.js
@@ -3,14 +3,14 @@ import { readFileSync, writeFileSync } from 'fs';
 
 // Release body
 {
-	const target = './package.json';
+	const target = './src-tauri/target/release/pokerogue.exe';
 	const args = {
 		'HASH': hash(),
 		'MD5': hashFile('md5', target),
 		'SHA1': hashFile('sha1', target),
 		'SHA256': hashFile('sha256', target),
 	};
-	let body = readFileSync('./src-tauri/target/release/pokerogue.exe', 'utf-8');
+	let body = readFileSync('./body-template.md', 'utf-8');
 	for (const key in args) body = body.replace(`{{${key}}}`, args[key]);
 	writeFileSync('./body.md', body);
 }
